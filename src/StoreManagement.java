e74ddf5ae0eb7b2a83004f9af7aed3265cf01fb4
Mahir Shariar <mahir35-2332@doi.edu.bd>
2020-04-13 00:18:49 +0600

    object creact hoitese nh premiumCustomer er jnno

---
+++ src/StoreManagement.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StoreManagement {
+    public static void main(String[] args) {
+/*
+        System.out.println("Enter Customer Type: ");
+        Scanner TypeOfCustomer = new Scanner(System.in);
+        String CustomerType = TypeOfCustomer.nextLine();
+        CustomerTypeFactory customerTypeFactory = new CustomerTypeFactory();
+        Customer Typename = CustomerTypeFactory.getCustomer(CustomerType);*/
+
+        PremiumCustomer customer = new PremiumCustomer();
+
+        /*
+
+        System.out.println("Customer Type: " + CustomerType);
+        System.out.println("Customer Id: " + CustomerType.getCustomerId());
+
+        */
+        System.out.println("Customer Total Payable Ammount: " + customer.getTotalPurchaseBalance());
+
+
+    }
+}
